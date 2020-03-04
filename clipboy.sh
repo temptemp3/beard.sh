30e05eb513e7235408b342ebfcc8881440abfcae
nicholas_s <nicholas_s@cbiz.co.jp>
2020-03-04 13:48:16 +0900

    stripdown, shift if say

--- clipboy.sh
+++ clipboy.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 ## clipboy
 ## - reads from clipboard and does things
-## version 0.1.2 - fix issue with help
+## version 0.1.3 - stripdown, shift on say
 ##################################################
 . ${SH2}/error.sh		# error handling
 error "true"			# show errors
 . ${SH2}/cecho.sh		# colored echo
 . ${SH2}/aliases/commands.sh	# commands
-##################################################
 generate-temp() { ${SH2}/generate-temp.sh ${@} ; }
-##################################################
 _cleanup() {
   {
     cecho yellow $( rm -rvf ${temp}-* )
   } 2>/dev/null
 }
-##################################################
 temp= 
 initialize() {
  temp=$( 
@@ -23,65 +20,9 @@ initialize() {
  )
  touch "${temp}-test"
 }
-##################################################
 clipboy-say() {
  cecho green "${@}"
 }
-##################################################
-clipboy-stretch() {
- clipboy-say $( clipboy-dialog stretch )
- touch "${temp}-clipboard"
- sleep 3
- clipboy-say "Me's stetchy!"
-}
-##################################################
-clipboy-run() {
- clipboy-say $( clipboy-dialog hi )
- clipboy-stretch
- sleep 2
- while [ ! ]
- do
-  clipboy-eat
-  clipboy-think
-  clipboy-act
-  ################################################
-  ## =notes=
-  ## (1) block may depend on runtime mode later
-  ################################################
-  [ ! ] && {
-   echo press enter key to continue
-   read
-  true
-  } || {
-   break
-  }
-  ################################################
-  clipboy-sleep
- done
- clipboy-say $( clipboy-dialog bye )
-}
-##################################################
-clipboy-sleep() {
- clipboy-say $( clipboy-dialog sleepy )
- sleep 5
-}
-##################################################
-clipboy-dialog() {
- . $( dirname ${0} )/${FUNCNAME}.sh
- test ! -f "$( dirname ${0} )/${FUNCNAME}-custom.sh" || {
-  . $( dirname ${0} )/${FUNCNAME}-custom.sh
- }
- commands
-}
-##################################################
-clipboy-act() {
- . $( dirname ${0} )/${FUNCNAME}.sh
- test ! -f "$( dirname ${0} )/${FUNCNAME}-custom.sh" || {
-  . $( dirname ${0} )/${FUNCNAME}-custom.sh
- }
- ${FUNCNAME}
-}
-##################################################
 clipboy-main() { 
   list-available-commands clipboy- \
   | sed -e 's/clipboy-//' \
@@ -89,6 +30,7 @@ clipboy-main() {
 }
 clipboy() { { local candidate_command ; candidate_command="$( echo ${1} | cut '-d:' '-f1' )" ; }
  initialize
+ test ! "${candidate_command}" = "say" || shift
  test -d "$( dirname ${0} )/commands" || mkdir -pv "${_}"
  local command_found
  for command_found in $( find $( dirname ${0} )/commands -type f -name "*-clipboy-${candidate_command}*.sh" )
